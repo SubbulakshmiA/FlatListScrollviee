6301e09426c578616ef67435b59e13c6a544292c
Subbulakshmi <subbulakshmi.arumugam20@gmail.com>
2024-01-17 16:10:46 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import MyFlatList from './Components/MyFlatList'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>New text</Text> */}
+      <MyFlatList></MyFlatList>
     </View>
   );
 }
@@ -14,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
 });
